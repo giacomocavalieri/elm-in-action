ddca8aea1d6647374cc50d24cfdc670f1a1933e5
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-04-14 15:30:14 +0200

    Chapter 8.1

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,100 @@
 module Main exposing (..)
 
-import PhotoGroove
+import Browser exposing (Document)
+import Html exposing (Html, a, footer, h1, li, nav, text, ul)
+import Html.Attributes exposing (classList, href)
+import Html.Events exposing (onMouseOver)
+import Html.Lazy exposing (lazy)
 
 
+
+-- MODEL
+
+
+type alias Model =
+    { page : Page }
+
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NothingYet
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view model =
+    let
+        content =
+            text "This isn't even my final form!"
+    in
+    { title = "Photo Groove, SPA Style"
+    , body =
+        [ lazy viewHeader model.page
+        , content
+        , viewFooter
+        ]
+    }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "Photo Groove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/gallery", caption = "Gallery" }
+                ]
+
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", page == targetPage ) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
 main =
-    PhotoGroove.main
+    Browser.document
+        { init = \_ -> ( { page = Gallery }, Cmd.none )
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
